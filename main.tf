46242d46d0c152c670747fd3f1680b63528890c7
Pedro Furtado <pedro.furtado@locaweb.com.br>
2024-04-07 19:25:23 -0300

    trigger ci

--- main.tf
+++ main.tf
@@ -1,51 +1,51 @@
-# resource "aws_iam_role" "my_role" {
-#   name = "test-role-created-by-terraform"
+resource "aws_iam_role" "my_role" {
+  name = "role_created_by_terraform"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Sid    = "foobar"
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = "foobar"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 
-#   inline_policy {
-#     name = "my_inline_policy"
+  inline_policy {
+    name = "my_inline_policy"
 
-#     policy = jsonencode({
-#       Version = "2012-10-17"
-#       Statement = [
-#         {
-#           Action   = ["ec2:*"]
-#           Effect   = "Allow"
-#           Resource = "*"
-#         },
-#       ]
-#     })
-#   }
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["ec2:*"]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
 
-#   inline_policy {
-#     name = "another_inline_policy"
+  inline_policy {
+    name = "another_inline_policy"
 
-#     policy = jsonencode({
-#       Version = "2012-10-17"
-#       Statement = [
-#         {
-#           Action   = ["iam:*"]
-#           Effect   = "Deny"
-#           Resource = "*"
-#         },
-#       ]
-#     })
-#   }
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["iam:*"]
+          Effect   = "Deny"
+          Resource = "*"
+        },
+      ]
+    })
+  }
 
-#   tags = {
-#     lorem = "ipsum"
-#   }
-# }
+  tags = {
+    lorem = "ipsum"
+  }
+}

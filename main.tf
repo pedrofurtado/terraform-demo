505cbbc9502df09d2b93a15aa639481f8865fd91
Pedro Furtado <pedro.furtado@locaweb.com.br>
2024-04-07 17:21:43 -0300

    fixes

--- main.tf
+++ main.tf
@@ -1,4 +1,36 @@
-resource "aws_instance" "my-machine-for-test-of-terraform" {
-  ami = "ami-2757f631"
-  instance_type = "t2.micro"
+resource "aws_iam_role" "my-role" {
+  name               = "test-role-created-by-terraform"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "my_inline_policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["ec2:Describe*"]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+
+  tags = {
+    tag-key = "tag-value"
+  }
 }

0c80ebde945ab72f628ddcd8f86865d784d8ca26
Pedro Furtado <pedro.furtado@locaweb.com.br>
2024-04-07 17:07:29 -0300

    changes

--- backend.tf
+++ backend.tf
@@ -1,7 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket = var.S3_BUCKET_NAME
-    key    = var.S3_KEY
-    region = var.AWS_REGION
-  }
+  backend "s3" {}
 }
--- variables.tf
+++ variables.tf
@@ -1,16 +1,3 @@
-variable "AWS_ACCESS_KEY" {
-  type = string
-}
-
-variable "AWS_SECRET_KEY" {
-  type = string
-}
-
-variable "AWS_REGION" {
-  type = string
-  # example: "us-east-1"
-}
-
 variable "S3_BUCKET_NAME" {
   type = string
   # example: "my-bucket-external-state-of-terraform"
